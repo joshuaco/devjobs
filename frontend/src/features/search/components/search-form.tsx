9afd6b384c651dd608ba54cd8b55c4fb43a10a46
Joshua <joxuecont@gmail.com>
2026-01-06 17:23:27 -0400

    feat: add clear button to search form

--- frontend/src/features/search/components/search-form.tsx
+++ frontend/src/features/search/components/search-form.tsx
@@ -1,9 +1,9 @@
-import { SearchIcon } from 'lucide-react';
+import { SearchIcon, XIcon } from 'lucide-react';
 import { useSearch } from '../hooks/useSearch';
 
 interface SearchFormProps {
   maxWidth?: string;
-  onSearch?: (search: string) => void;
+  onSearch: (search: string) => void;
 }
 
 function SearchForm({ maxWidth = 'max-w-2xl', onSearch }: SearchFormProps) {
@@ -15,29 +15,40 @@ function SearchForm({ maxWidth = 'max-w-2xl', onSearch }: SearchFormProps) {
     const formData = new FormData(e.target as HTMLFormElement);
     const search = formData.get('search') as string;
 
-    onSearch?.(search);
+    onSearch(search);
   };
 
   return (
     <form
-      role='search'
+      role="search"
       className={`flex mx-auto w-full ${maxWidth} justify-center`}
       onSubmit={handleSubmit}
     >
-      <div className='flex gap-2 items-center bg-input rounded-md p-2 w-full shadow-md pl-4'>
-        <SearchIcon className='w-6 h-6 text-muted' />
+      <div className="flex gap-2 items-center bg-input rounded-md p-2 w-full shadow-md pl-4">
+        <SearchIcon className="w-6 h-6 text-muted" />
         <input
-          type='text'
-          name='search'
-          placeholder='Buscar empleos por título, habilidad o empresa'
-          className='w-full bg-transparent outline-none text-white p-1'
+          type="text"
+          name="search"
+          placeholder="Buscar empleos por título, habilidad o empresa"
+          className="w-full bg-transparent outline-none text-white p-1"
           value={search}
           onChange={(e) => handleSearch(e.target.value)}
           required
         />
+        {search && (
+          <button
+            type="button"
+            className="bg-gray-600 rounded-full p-1"
+            onClick={() => {
+              onSearch('');
+            }}
+          >
+            <XIcon className="w-4 h-4 text-white" />
+          </button>
+        )}
         <button
-          type='submit'
-          className='bg-primary-hover text-white px-5 py-2 rounded-md text-sm font-medium'
+          type="submit"
+          className="bg-primary-hover text-white px-5 py-2 rounded-md text-sm font-medium"
         >
           Buscar
         </button>
