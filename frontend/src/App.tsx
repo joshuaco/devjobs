a061ad8dd8acd885735e954d02d20eae0257c479
Joshua <joxuecont@gmail.com>
2026-02-01 19:06:34 -0400

    refactor: convert components to lazy-loaded imports for improved performance

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
-import HomePage from '@/pages/home-page';
-import JobPage from './pages/job-page';
-import SearchPage from '@/pages/jobs-page';
-import MainLayout from '@/layouts/main';
 import { Routes, Route } from 'react-router';
+import { lazy } from 'react';
+
+const HomePage = lazy(() => import('@/pages/home-page'));
+const SearchPage = lazy(() => import('@/pages/jobs-page'));
+const JobPage = lazy(() => import('@/pages/job-page'));
+const MainLayout = lazy(() => import('@/layouts/main'));
 
 function App() {
   return (
