81ebb1808507b0fc5db9e5ad480ebfdd7cc910f8
Joshua <joxuecont@gmail.com>
2026-04-02 11:36:07 -0400

    feat: enhance job retrieval with filtering options and pagination

--- backend/src/controllers/jobs.ts
+++ backend/src/controllers/jobs.ts
@@ -1,8 +1,51 @@
 import jobs from '../data/jobs.json' with { type: 'json' };
 import type { Request, Response } from 'express';
+import type { JobQueryParams } from '../types/jobs.ts';
 
-const getJobs = (_req: Request, res: Response) => {
-  return res.json({ jobs });
+const getJobs = (req: Request, res: Response) => {
+  const {
+    search,
+    experience,
+    technology,
+    location,
+    limit = '10',
+    offset = '0',
+  } = req.query as JobQueryParams;
+
+  let filteredJobs = jobs;
+
+  const limitNumber = parseInt(limit);
+  const offsetNumber = parseInt(offset);
+
+  if (search) {
+    const searchTerm = search.toLowerCase();
+
+    filteredJobs = filteredJobs.filter(
+      (job) =>
+        job.titulo.toLowerCase().includes(searchTerm) ||
+        job.descripcion.toLowerCase().includes(searchTerm)
+    );
+  }
+
+  if (experience) {
+    filteredJobs = filteredJobs.filter((job) => job.data.nivel === experience);
+  }
+
+  if (technology) {
+    filteredJobs = filteredJobs.filter((job) =>
+      job.data.technology.includes(technology)
+    );
+  }
+
+  if (location) {
+    filteredJobs = filteredJobs.filter((job) => job.data.modalidad === location);
+  }
+
+  const total = filteredJobs.length;
+
+  filteredJobs = filteredJobs.slice(offsetNumber, offsetNumber + limitNumber);
+
+  return res.json({ jobs: filteredJobs, total });
 };
 
 const getJobById = (req: Request, res: Response) => {
@@ -11,7 +54,7 @@ const getJobById = (req: Request, res: Response) => {
   // Invalid id format using RegExp
   if (
     !/^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/.test(
-      id as string,
+      id as string
     )
   ) {
     return res.status(400).json({
---
+++ backend/src/types/jobs.ts
@@ -0,0 +1,8 @@
+export type JobQueryParams = {
+  search?: string;
+  experience?: string;
+  technology?: string;
+  location?: string;
+  limit: string;
+  offset: string;
+};
